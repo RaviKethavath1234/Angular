2c11dd308778dc38447445aabb2a0f3877e6b625
RaviKethavath1234 <ravikethavath70@gmail.com>
2025-01-26 13:06:20 +0530

    First version

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone:true,
+  selector: 'app-root',
+  imports: [RouterOutlet,FormsModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+ city="HYD";
+ UpdateCity(){
+  this.city="Pune"
+ };
+
+ headings=["ID","ENAME","SAL"];
+ Evalues=["01","Ravi K","10000","button"];
+
+data=false;
+num=["1","2","3","4","5"];
+
+num1:number=10;
+num2:number=12;
+
+operation:string="-";
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  date: string = new Date().toLocaleDateString();
+  isActive:boolean=false; 
+  cvar:string='green'
+
+  counter: number = 0; // Initialize the counter
+
+  Increament(): void {
+    this.counter++; // Increment the counter
+  }
+
+  Decreament(): void {
+    this.counter--; // Decrement the counter
+  }
+}
